d1ab412910b83b7cb800e88417cdb897755a20f5
Andrew Bissell <abissell@fake.address>
2013-03-22 04:31:09 -0500

    Created HelloWorld

---
+++ src/chapter2/HelloWorld.scala
@@ -0,0 +1,19 @@
+package chapter2
+
+object HelloWorld {
+	def main(args:Array[String]) {
+		val msg = "Hello, world!"
+		// Java typedef val
+		val msg2: java.lang.String = "Hello again, world!"
+		// Shorter Java typedef val
+		val msg3: String = "Hello yet again, world!"
+
+		println(msg)
+
+		// Reassignment to val unallowed by compiler
+		// msg = "Goodbye cruel world!"
+
+		var greeting = "Hello, world!"
+		greeting = "Leave me alone, world!"
+	}
+}
